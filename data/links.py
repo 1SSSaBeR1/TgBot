03964f589370d4c2f8ae8ed0e149da1c0113aad7
1SSSaBeR1 <151942425+1SSSaBeR1@users.noreply.github.com>
2024-04-28 17:53:59 +0300

    Update links.py

--- data/links.py
+++ data/links.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from .db_session import SqlAlchemyBase
 
 
@@ -7,7 +7,8 @@ from .db_session import SqlAlchemyBase
 class Link(SqlAlchemyBase):
     __tablename__ = 'links'
 
-    id = Column(Integer, primary_key=True)
-    url = Column(String)
-    description = Column(String)
-    post = relationship('blogs', backref='links')
+    id: Mapped[int] = mapped_column(primary_key=True)
+    url: Mapped[str]
+    description: Mapped[str]=mapped_column(nullable=True)
+    is_complited: Mapped[bool] = mapped_column(server_default=False)
+    #post = relationship('blogs', backref='links')
